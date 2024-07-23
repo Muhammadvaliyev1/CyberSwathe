e17b9b8be043f9b29aa03813c4cb880b2588b09a
Muhammadvaliyev1 <127742778+Muhammadvaliyev1@users.noreply.github.com>
2024-07-23 11:21:16 +0500

    addded new details

--- public/app.js
+++ public/app.js
@@ -27,3 +27,71 @@ document.querySelectorAll('.btn').forEach(button => {
         document.getElementById(button.getAttribute('data-content')).classList.add('grid');
     });
 });
+
+document.getElementById('aside-button').addEventListener('click', function() {
+    const aside = document.getElementById('aside');
+    const main = document.getElementById('main');
+    const asbutton = document.getElementById('asbutton');
+
+    // Toggle classes on aside
+    if (aside.classList.contains('max-w-[0px]')) {
+        aside.classList.remove('max-w-[0px]');
+    } else {
+        aside.classList.add('max-w-[0px]');
+    }
+
+    // Toggle classes on main
+    if (main.classList.contains('w-full')) {
+        main.classList.remove('w-full');
+    } else {
+        main.classList.add('w-full');
+    }
+    
+    if (asbutton.classList.contains('-rotate-180')) {
+        asbutton.classList.remove('-rotate-180');
+    } else {
+        asbutton.classList.add('-rotate-180');
+    }
+});
+
+
+function showDropdownOptions() {
+    document.getElementById("options").classList.toggle("hidden");
+    document.getElementById("arrow-up").classList.toggle("hidden");
+    document.getElementById("arrow-down").classList.toggle("hidden");
+}
+
+function showDropdownOptions1() {
+    document.getElementById("options1").classList.toggle("hidden");
+    document.getElementById("arrow-up").classList.toggle("hidden");
+    document.getElementById("arrow-down").classList.toggle("hidden");
+}
+function action() {
+    document.getElementById("action").classList.toggle("hidden");
+    document.getElementById("arrow-up").classList.toggle("hidden");
+    document.getElementById("arrow-down").classList.toggle("hidden");
+}
+
+function toggleAccordion(button) {
+    const allContents = document.querySelectorAll('.accordion-content');
+    const allButtons = document.querySelectorAll('button[type="button"] svg');
+    const content = button.nextElementSibling;
+    const svg = button.querySelector('svg');
+
+    allContents.forEach(c => {
+        if (c !== content) {
+            c.classList.add('hidden');
+            c.classList.remove('show');
+        }
+    });
+
+    allButtons.forEach(s => {
+        if (s !== svg) {
+            s.classList.remove('rotate-180');
+        }
+    });
+
+    content.classList.toggle('hidden');
+    content.classList.toggle('show');
+    svg.classList.toggle('rotate-180');
+}
